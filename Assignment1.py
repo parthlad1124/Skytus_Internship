775f56e416ebaeb7dc805a5d957b82776fcac949
parthlad1124 <153207555+parthlad1124@users.noreply.github.com>
2026-01-09 10:43:26 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,42 @@
+# print("Name: Parth, Age: 21, City: Navsari")
+
+# a = int(input("Enter a:"))
+# b = int(input("Enter b:"))
+# print(f"Sum is {a+b}")
+
+# celsius = float(input("Enter temp in celsius:"))
+# fahrenheit = (celsius * 9/5) +32
+# print("Temperature in fahrenheit:", fahrenheit)
+
+# name = "Parth"
+# print(name.upper())
+
+# BD = int(input("Enter your birth year:"))
+# current_year = 2026
+# age = current_year - BD
+# print(f"Your age is {age}")
+
+# a = int(input("Enter a:"))
+# b = int(input("Enter b:"))
+# a,b = b,a
+# print("a is ",a)
+# print("b is ",b)
+
+# l = float(input("Enter length: "))
+# w = float(input("Enter width: "))
+# area = l*w
+# print("Area of rectangle is ", area)
+
+# num = int(input("Enter a number:"))
+# if num>=0:
+#     print("Number is positive")
+# else:
+#     print("Number is negative")
+
+# x = int(input("Enter x:"))
+# y = int(input("Enter y:"))
+# average = (x+y)/2
+# print("Average is ", average)
+
+colour = input("Enter your favourite colour:")
+print(f"My favourite colour is {colour}")
